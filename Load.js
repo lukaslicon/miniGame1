9998d482560b1e0d42ffda916b2b3d434e6f646d
Lukas Licon <lukas@licons.com>
2023-05-23 16:36:47 -0700

    load images

--- Load.js
+++ Load.js
@@ -4,7 +4,12 @@ class Load extends Phaser.Scene {
     }
     preload ()
     {
-        this.load.path = 'assets/';
+        this.load.path = 'assets/images';
+        this.load.image('ball', 'ball.png');
+        this.load.image('coin', 'coin.png');
+        this.load.image('bob', 'bob.png');
+        this.load.image('background1', 'game1background.png');
+        this.load.image('summaryScreen', 'summaryscreen.png');
     }
     create()
     {
