484269285eb136df92cbbf205803b921c190f9de
Lukas Licon <lukas@licons.com>
2023-05-23 18:57:15 -0700

    working on collisions

--- MiniGame.js
+++ MiniGame.js
@@ -7,33 +7,40 @@ class MiniGame extends Phaser.Scene {
     {
         //player rectangle
         this.player = this.physics.add.image(960, 590, 'bob').setScale(1.5);
+        this.player.body.setCollideWorldBounds(true); 
+
+        //keyboard input
         this.cursors = this.input.keyboard.createCursorKeys();
         this.cursors = this.input.keyboard.addKeys(
             {up:Phaser.Input.Keyboard.KeyCodes.W,
             down:Phaser.Input.Keyboard.KeyCodes.S,
             left:Phaser.Input.Keyboard.KeyCodes.A,
             right:Phaser.Input.Keyboard.KeyCodes.D});
-        //wall squares
 
+
+        //wall 
         this.group = this.physics.add.staticGroup({
-            key: 'bob',
+            key: 'boundary',
             frameQuantity: 50
         });
-        Phaser.Actions.PlaceOnRectangle(this.group.getChildren(), new Phaser.Geom.Rectangle(0, 0, 1920, 1080));
+        
+        Phaser.Actions.PlaceOnRectangle(
+            this.group.getChildren(), 
+            new Phaser.Geom.Rectangle(0, 0, 1920, 1080)
+            );
 
-        this.player.body.setCollideWorldBounds(true); //player cannot go out of bounds
+        this.physics.add.collider(this.player, this.group);
+        
         //controls
         this.add.text(50, 50, "Movement:").setFontSize(30);
         this.add.text(225, 35, "  W").setFontSize(25);
         this.add.text(225, 65, "A S D").setFontSize(25);
-
-
-
+       
     }
     update ()
     {
+        
         this.player.body.setVelocity(0);
-
         if (this.cursors.left.isDown)
         {
             this.player.body.setVelocityX(-playerVelocity);
