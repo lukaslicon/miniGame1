fb5d5658d6ae1a179c03f7561b80dbda82ea0ba1
Lukas Licon <lukas@licons.com>
2023-05-23 17:44:34 -0700

    working so far

--- MiniGame.js
+++ MiniGame.js
@@ -1,23 +1,32 @@
+playerVelocity = 2000;
 class MiniGame extends Phaser.Scene {
     constructor() {
         super('MiniGame')
     }
-
     create()
     {
-        this.add.image(960, 540, 'background1');
-        
         //player rectangle
-        this.player = this.physics.add.image(400, 300, 'bob').setScale(.75);
+        this.player = this.physics.add.image(960, 590, 'bob').setScale(1.5);
         this.cursors = this.input.keyboard.createCursorKeys();
+        this.cursors = this.input.keyboard.addKeys(
+            {up:Phaser.Input.Keyboard.KeyCodes.W,
+            down:Phaser.Input.Keyboard.KeyCodes.S,
+            left:Phaser.Input.Keyboard.KeyCodes.A,
+            right:Phaser.Input.Keyboard.KeyCodes.D});
+        //wall squares
+
+        this.group = this.physics.add.staticGroup({
+            key: 'bob',
+            frameQuantity: 50
+        });
+        Phaser.Actions.PlaceOnRectangle(this.group.getChildren(), new Phaser.Geom.Rectangle(0, 0, 1920, 1080));
+
         this.player.body.setCollideWorldBounds(true); //player cannot go out of bounds
-
-        this.coins = this.physics.add.group({ immovable: true });
-        const outer = new Phaser.Geom.Rectangle(50, 50, 1800, 1200);
-        const inner = new Phaser.Geom.Rectangle(800, 300, 500, 200);
-
         //controls
-        this.add.text(50, 50, "Movement: Arrow Keys\n\nDont get hit!").setFontSize(30)
+        this.add.text(50, 50, "Movement:").setFontSize(30);
+        this.add.text(225, 35, "  W").setFontSize(25);
+        this.add.text(225, 65, "A S D").setFontSize(25);
+
 
 
     }
@@ -27,22 +36,19 @@ class MiniGame extends Phaser.Scene {
 
         if (this.cursors.left.isDown)
         {
-            this.player.body.setVelocityX(-300);
+            this.player.body.setVelocityX(-playerVelocity);
         }
         else if (this.cursors.right.isDown)
         {
-            this.player.body.setVelocityX(300);
+            this.player.body.setVelocityX(playerVelocity);
         }
         if (this.cursors.up.isDown)
         {
-            this.player.body.setVelocityY(-300);
+            this.player.body.setVelocityY(-playerVelocity);
         }
         else if (this.cursors.down.isDown)
         {
-            this.player.body.setVelocityY(300);
+            this.player.body.setVelocityY(playerVelocity);
         }
         }
     }
-
-    
-    
--- load.js
+++ load.js
@@ -7,7 +7,7 @@ class load extends Phaser.Scene {
         this.load.path = 'assets/images/';
         this.load.image('summaryScreen', 'summaryscreen.png');
         this.load.image('bob', 'bob.png');
-        this.load.image('background1', 'game1background.png');
+
     }
     create()
     {
